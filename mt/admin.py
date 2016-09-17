fcbaa7bf46ccb4e2920b3e864aab7b59d560f611
Dave Brookshire <dave@brookshire.org>
2016-09-17 08:26:20 -0500

    Started to define the Admin models.

--- mt/admin.py
+++ mt/admin.py
@@ -4,8 +4,45 @@
 from django.contrib import admin
 from .models import Asset, AssetTransaction, Person, Genre
 
-# Register your models here.
-admin.site.register(Asset)
+
+class DirectorInline(admin.StackedInline):
+    model = Person
+
+
+class ActorInline(admin.StackedInline):
+    model = Person
+
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'upc', 'genre',)
+    list_filter = ('genre',)
+    ordering = ('title', 'upc')
+    # inlines = [
+    #     DirectorInline,
+    #     ActorInline,
+    # ]
+    save_as = True
+    fieldsets = (
+        ("Title Values", {
+            'fields': ('title', 'upc', 'asin', 'url',
+                       'release_date', 'publication_date',
+                       'genre')
+        },),
+        ("Misc", {
+            # 'classes': ('collapse', ),
+            'fields': ('binding', 'brand', 'features',
+                       'price', 'list_price', 'currency',
+                       'sales_rank'),
+        }),
+        ("Images", {
+            'classes': ('collapse', ),
+            'fields': ('large_img', 'medium_img', 'small_img'),
+        }),
+    )
+
+
+# admin.site.register(Asset)
 admin.site.register(AssetTransaction)
 admin.site.register(Person)
 admin.site.register(Genre)
